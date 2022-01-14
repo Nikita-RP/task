165652b336fb297540d0919dd3e8c18ecb180d19
Nikita-RP <rajpurohitnikita0808@gmail.com>
2022-01-14 23:36:08 +0530

    first comm

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<?php
+if(!(isset($_GET['a']) || isset($_GET['A']))){
+    echo "Please pass value for index: A";
+    exit;
+}
+
+if(!(isset($_GET['b']) || isset($_GET['B']))){
+    echo "Please pass value for index: B";
+    exit;
+}
+
+if(!(isset($_GET['c']) || isset($_GET['C']))){
+    echo "Please pass value for index: C";
+    exit;
+}
+$Sum exec('python C:\xampp\htdocs\Surbhi_programe\first.py ' . implode(' ', $_GET)); //getting value from python after addition
+print("Total is $sum");
+
+
+?>
